38c0f460c9ae45f6862ee512676ff1ce4f98faf8
AbilashPL <abhilash143.chintu@gmail.com>
2021-04-07 14:57:07 +0530

    Commit2

---
+++ src/test/java/TestGroup/GitToJenkins3/TestClass3.java
@@ -0,0 +1,38 @@
+package TestGroup.GitToJenkins3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass3 {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void beforeMethodTesting() {
+		System.out.println("Before Method");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Abilash PL\\Desktop\\SeleniumProjects\\drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void testing1() {
+		System.out.println("In Test");
+
+		driver.navigate().to("http://www.javatpoint.com/");
+		driver.manage().window().maximize();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("scrollBy(0, 5000)");
+		driver.findElement(By.linkText("Core Java")).click();
+
+		System.out.println(".............TC2.......");
+	}
+	@Test
+	public void main3() {
+		System.out.println("<<<<<<<TC3........");
+	}
+}
